d903cf23e2fe51adfde4b5f0b8f714901b9f8a8f
AKENA NICHOLAS <akidirita4@gmail.com>
2026-04-17 17:00:51 +0300

    Refactor imports and update __all__ in __init__.py

--- data_buddy/__init__.py
+++ data_buddy/__init__.py
@@ -1,1 +1,13 @@
-from .project_analyzer import get_average, get_median, get_min, get_max, get_mode, add, sub, mul, div, power, sqrt, run_no_code_analysis, generate_notebook_code, create_visual_report 
+ from .project_analyzer import (
+    get_average, get_median, get_min, get_max, get_mode, 
+    add, sub, mul, div, power, sqrt, 
+    run_no_code_analysis, generate_notebook_code, create_visual_report
+)
+from .statistics_engine import StatisticalAnalyzer
+
+__all__ = [
+    'get_average', 'get_median', 'get_min', 'get_max', 'get_mode',
+    'add', 'sub', 'mul', 'div', 'power', 'sqrt',
+    'run_no_code_analysis', 'generate_notebook_code', 'create_visual_report',
+    'StatisticalAnalyzer'
+]
